3345ffea80f48f394ed1796292c6ca4e32f0e9ef
Stephany <stephany.tec@hotmail.com>
2022-07-13 16:06:20 -0300

    Adicionado o jogo da velha completo

---
+++ JogoDaVelha/JogoDaVelha/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace JogoDaVelha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(23, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 54);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(104, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 54);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(185, 28);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 54);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(23, 99);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 54);
+            this.button4.TabIndex = 2;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(104, 99);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 54);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(185, 99);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 54);
+            this.button6.TabIndex = 4;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(23, 169);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 54);
+            this.button7.TabIndex = 8;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(104, 169);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 54);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(185, 169);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 54);
+            this.button9.TabIndex = 6;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(77, 312);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(102, 34);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Reset";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "P1 Vitórias";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "P2 VItórias";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Empates";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 274);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(101, 274);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(182, 274);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 358);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da Velha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ JogoDaVelha/JogoDaVelha/Form1.cs
@@ -0,0 +1,433 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Global
+        {
+            public static bool turn, button_disable;
+            public static int player1_wins = 0, player2_wins = 0, tie = 0, rounds = 0, p1 = 0, 
+            A = 0, B = 0, C = 0, D = 0, E = 0, F = 0, G = 0, H = 0, I = 0;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //verficando jogada do player 1
+            if(Global.turn == false && Global.button_disable ==  false && Global.A == 0 )
+            {
+                button1.ForeColor = Color.Red;
+                button1.Text = "X";
+                Global.A = 1;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = true;
+              
+            }
+
+            //verificando jogada do player 2
+            if (Global.turn == true && Global.button_disable == false && Global.A == 0)
+            {
+                button1.ForeColor = Color.Blue;
+                button1.Text = "O"; 
+                Global.A = 2;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = false;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //verficando jogada do player 1
+            if (Global.turn == false && Global.button_disable == false && Global.B == 0)
+            {
+                button2.ForeColor = Color.Red;
+                button2.Text = "X";
+                Global.B = 1;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = true;
+
+            }
+
+            //verificando jogada do player 2
+            if (Global.turn == true && Global.button_disable == false && Global.B == 0)
+            {
+                button2.ForeColor = Color.Blue;  
+                button2.Text = "O";
+                Global.B = 2;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = false;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //verficando jogada do player 1
+            if (Global.turn == false && Global.button_disable == false && Global.I == 0)
+            {
+                button9.ForeColor = Color.Red;
+                button9.Text = "X";
+                Global.I = 1;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = true;
+
+            }
+
+            //verificando jogada do player 2
+            if (Global.turn == true && Global.button_disable == false && Global.I == 0)
+            {
+                button9.ForeColor = Color.Blue;
+                button9.Text = "O";
+                Global.I = 2;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //verficando jogada do player 1
+            if (Global.turn == false && Global.button_disable == false && Global.D == 0)
+            {
+                button4.ForeColor = Color.Red;
+                button4.Text = "X";
+                Global.D = 1;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = true;
+
+            }
+
+            //verificando jogada do player 2
+            if (Global.turn == true && Global.button_disable == false && Global.D == 0)
+            {
+                button4.ForeColor = Color.Blue;
+                button4.Text = "O";
+                Global.D = 2;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //verficando jogada do player 1
+            if (Global.turn == false && Global.button_disable == false && Global.C == 0)
+            {   
+                button3.ForeColor = Color.Red;
+                button3.Text = "X";
+                Global.C = 1;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = true;
+
+            }
+
+            //verificando jogada do player 2
+            if (Global.turn == true && Global.button_disable == false && Global.C == 0)
+            {
+                button3.ForeColor = Color.Blue;
+                button3.Text = "O";
+                Global.C = 2;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = false;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //verficando jogada do player 1
+            if (Global.turn == false && Global.button_disable == false && Global.E == 0)
+            {
+                button5.ForeColor = Color.Red;
+                button5.Text = "X";
+                Global.E = 1;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = true;
+
+            }
+
+            //verificando jogada do player 2
+            if (Global.turn == true && Global.button_disable == false && Global.E == 0)
+            {
+                button5.ForeColor = Color.Blue;
+                button5.Text = "O";
+                Global.E = 2;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = false;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //verficando jogada do player 1
+            if (Global.turn == false && Global.button_disable == false && Global.F == 0)
+            {
+                button6.ForeColor = Color.Red;
+                button6.Text = "X";
+                Global.F = 1;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = true;
+
+            }
+
+            //verificando jogada do player 2
+            if (Global.turn == true && Global.button_disable == false && Global.F == 0)
+            {
+                button6.ForeColor = Color.Blue;
+                button6.Text = "O";
+                Global.F = 2;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = false;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //verficando jogada do player 1
+            if (Global.turn == false && Global.button_disable == false && Global.G == 0)
+            {
+                button7.ForeColor = Color.Red;
+                button7.Text = "X";
+                Global.G = 1;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = true;
+
+            }
+
+            //verificando jogada do player 2
+            if (Global.turn == true && Global.button_disable == false && Global.G == 0)
+            {
+                button7.ForeColor = Color.Blue;
+                button7.Text = "O";
+                Global.G = 2;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = false;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //verficando jogada do player 1
+            if (Global.turn == false && Global.button_disable == false && Global.H == 0)
+            {
+                button8.ForeColor = Color.Red;
+                button8.Text = "X";
+                Global.H = 1;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = true;
+
+            }
+
+            //verificando jogada do player 2
+            if (Global.turn == true && Global.button_disable == false && Global.H == 0)
+            {
+                button8.ForeColor = Color.Blue;
+                button8.Text = "O";
+                Global.H = 2;
+                Global.rounds++;
+                CheckingWinner();
+                Global.turn = false;
+            }
+        }
+
+        private void wins()
+        {
+            if (Global.p1 == 1)
+            {
+                Global.player1_wins++;
+                label4.Text = Convert.ToString(Global.player1_wins);
+                MessageBox.Show("Jogador 1 venceu!");
+                Global.button_disable = true;
+            }
+            else if (Global.p1 == 2)
+            {
+                Global.player2_wins++;
+                label5.Text = Convert.ToString(Global.player2_wins);
+                MessageBox.Show("Jogador 2 venceu!");
+                Global.button_disable = true;
+            }
+
+        }
+        private void CheckingWinner()
+        {
+            // Verificações de vitórias do jogador 1(X) nas horizontais:
+            if (Global.A == 1 && Global.B == 1 && Global.C == 1)
+            {
+                Global.p1 = 1;
+                wins();
+            }
+
+            if (Global.D == 1 && Global.E == 1 && Global.F == 1)
+            {
+                Global.p1 = 1;
+                wins();
+            }
+
+            if (Global.G == 1 && Global.H == 1 && Global.I == 1)
+            {
+                Global.p1 = 1;
+                wins();
+            }
+
+            // Verificações de vitórias do jogador 1(X) nas verticais:
+            if (Global.A == 1 && Global.D == 1 && Global.G == 1)
+            {
+                Global.p1 = 1;
+                wins();
+            }
+
+            if (Global.B == 1 && Global.E == 1 && Global.H == 1)
+            {
+                Global.p1 = 1;
+                wins();
+            }
+
+            if (Global.C == 1 && Global.F == 1 && Global.I == 1)
+            {
+                Global.p1 = 1;
+                wins();
+            }
+
+            // Verificações de vitórias do jogador 1(X) nas diagonais:
+            if (Global.A == 1 && Global.E == 1 && Global.I == 1)
+            {
+                Global.p1 = 1;
+                wins();
+            }
+
+            if (Global.C == 1 && Global.E == 1 && Global.G == 1)
+            {
+                Global.p1 = 1;
+                wins();
+            }
+
+            // Verificações de vitórias do jogador 2(O) nas horizontais:
+            if (Global.A == 2 && Global.B == 2 && Global.C == 2)
+            {
+                Global.p1 = 2;
+                wins();
+            }
+
+            if (Global.D == 2 && Global.E == 2 && Global.F == 2)
+            {
+                Global.p1 = 2;
+                wins();
+            }
+
+            if (Global.G == 2 && Global.H == 2 && Global.I == 2)
+            {
+                Global.p1 = 2;
+                wins();
+            }
+
+            // Verificações de vitórias do jogador 2(O) nas verticais:
+            if (Global.A == 2 && Global.D == 2 && Global.G == 2)
+            {
+                Global.p1 = 2;
+                wins();
+            }
+
+            if (Global.B == 2 && Global.E == 2 && Global.H == 2)
+            {
+                Global.p1 = 2;
+                wins();
+            }
+
+            if (Global.C == 2 && Global.F == 2 && Global.I == 2)
+            {
+                Global.p1 = 2;
+                wins();
+            }
+
+            // Verificações de vitórias do jogador 2(O) nas diagonais:
+            if (Global.A == 2 && Global.E == 2 && Global.I == 2)
+            {
+                Global.p1 = 2;
+                wins();
+            }
+
+            if (Global.C == 2 && Global.E == 2 && Global.G == 2)
+            {
+                Global.p1 = 2;
+                wins();
+            }
+
+            //verifica se deu empate
+            if (Global.p1 == 0 && Global.rounds == 9)
+            {
+                Global.tie++;
+                label6.Text = Convert.ToString(Global.tie);
+                MessageBox.Show("Empate");
+                Global.button_disable = true;
+
+            }
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Global.A = 0;
+            Global.B = 0;
+            Global.C = 0;
+            Global.D = 0;
+            Global.E = 0;
+            Global.F = 0;
+            Global.G = 0;
+            Global.H = 0;
+            Global.I = 0;
+            Global.rounds = 0;
+            button1.Text = " ";
+            button2.Text = " ";
+            button3.Text = " ";
+            button4.Text = " ";
+            button4.Text = " ";
+            button5.Text = " ";
+            button6.Text = " ";
+            button7.Text = " ";
+            button8.Text = " ";
+            button9.Text = " ";
+            Global.button_disable = false;
+
+            if (Global.p1 == 1 || Global.p1 == 0)
+            {
+                Global.turn = false;
+                Global.p1 = 0;
+            }
+            else if (Global.p1 == 2)
+            {
+                Global.turn = true;
+                Global.p1 = 0;
+            }
+        
+        }
+    }
+      
+
+ }
